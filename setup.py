c85fce8a457ad11094eafaf1a5e187bba0602a5a
Taishi Hashimoto <hashimoto.taishi@outlook.com>
2022-11-17 22:58:23 +0900

    chore: release (v1.1.0)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ package_dir = "src"
 
 setup(
     name='signalgenerator',
-    version="v1.0.0",
+    version="v1.1.0",
     description="Simple signal generator (emulator).",
     package_dir={
         "": package_dir
